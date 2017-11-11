5409449af94ed4ae855c2b0d4dd0211c5b7cf18e
Tommy Brunn <tommy.brunn@gmail.com>
2017-11-11 15:28:47 +0100

    Refactor response validation.
    Fixes #7 by introducing more granular error messaging for response
    objects. Signature is unchanged, but the InvalidHealthcheckResponse
    error message will contain more information about what exactly is wrong
    with the response.

--- src/response/response.js
+++ src/response/response.js
@@ -3,38 +3,34 @@ const R = require("ramda");
 const InvalidHealthcheckResponse = require("./invalid-healthcheck-response");
 const validate = require("./validate");
 
+const RESPONSE_FORMAT = {
+  name: undefined,
+  healthy: undefined,
+  actionable: undefined,
+  type: undefined,
+  severity: undefined,
+  message: undefined,
+  dependentOn: undefined,
+  info: undefined,
+  link: undefined
+};
+
 const createResponse = responseData =>
-  R.pick(
-    [
-      "name",
-      "healthy",
-      "actionable",
-      "type",
-      "severity",
-      "message",
-      "dependentOn",
-      "info",
-      "link"
-    ],
-    responseData
-  );
+  R.pick(Object.keys(RESPONSE_FORMAT), responseData);
+
+const someFail = results =>
+  R.not(R.all(R.equals(true), Object.values(results)));
+
+const isMessage = R.compose(R.not, R.is(Boolean));
+const firstMessage = R.compose(R.head, R.filter(isMessage), R.values);
 
 module.exports = function(responseData) {
-  if (
-    !validate(
-      responseData.name,
-      responseData.healthy,
-      responseData.actionable,
-      responseData.type,
-      responseData.severity,
-      responseData.message,
-      responseData.dependentOn,
-      responseData.info,
-      responseData.link
-    )
-  ) {
+  const validatable = Object.assign({}, RESPONSE_FORMAT, responseData);
+  const validationResult = validate(validatable);
+
+  if (someFail(validationResult)) {
     throw new InvalidHealthcheckResponse(
-      "Invalid input for HealthCheckResponse",
+      firstMessage(validationResult),
       responseData
     );
   }
