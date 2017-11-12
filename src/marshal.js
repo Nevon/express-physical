43b70ecf21b16d8207cc0dd68118daad2f4724ba
Tommy Brunn <tommy.brunn@gmail.com>
2017-11-12 20:28:39 +0100

    Remove unnecessary currying

--- src/marshal.js
+++ src/marshal.js
@@ -6,7 +6,7 @@ const serialize = require("./response/serialize");
 const byHealth = R.groupBy(
   response => (response.healthy ? "healthy" : "unhealthy")
 );
-const serializeAll = R.curry(R.map)(serialize);
+const serializeAll = R.map(serialize);
 const addRequiredEmptyGroups = response =>
   Object.assign(
     {
--- src/response/serialize.js
+++ src/response/serialize.js
@@ -2,9 +2,11 @@
 const R = require("ramda");
 const snakeCaseKeys = require("snakecase-keys");
 
+const nonEmpty = R.compose(R.not, R.either(R.isNil, R.isEmpty));
+
 const removeEmptyFields = function(obj) {
   return Object.keys(obj).reduce((acc, field) => {
-    if (R.compose(R.not, R.either(R.isNil, R.isEmpty))(obj[field])) {
+    if (nonEmpty(obj[field])) {
       acc[field] = obj[field];
     }
 
@@ -12,9 +14,16 @@ const removeEmptyFields = function(obj) {
   }, {});
 };
 
-const formatDependentOn = R.when(
-  R.compose(R.curry(R.is(String)), R.curry(R.prop("dependentOn"))),
-  obj => Object.assign(obj, { dependentOn: { serviceName: obj.dependentOn } })
+const dependentOnPresent = R.compose(R.is(String), R.prop("dependentOn"));
+const formatDependentOn = obj =>
+  Object.assign(obj, { dependentOn: { serviceName: obj.dependentOn } });
+const formatDependentOnIfPresent = R.when(
+  dependentOnPresent,
+  formatDependentOn
 );
 
-module.exports = R.compose(snakeCaseKeys, formatDependentOn, removeEmptyFields);
+module.exports = R.compose(
+  snakeCaseKeys,
+  formatDependentOnIfPresent,
+  removeEmptyFields
+);
