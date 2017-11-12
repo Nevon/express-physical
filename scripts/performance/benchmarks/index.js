1e90a00587069296d791f70284cb2e97765c21a8
Tommy Brunn <tommy.brunn@gmail.com>
2017-11-12 22:09:41 +0100

    Add benchmarks for middleware and marshalling

--- scripts/performance/benchmarks/index.js
+++ scripts/performance/benchmarks/index.js
@@ -1,15 +1,19 @@
 "use strict";
 const Benchmark = require("benchmark");
 const bb = require("beautify-benchmark");
+const request = require("supertest");
+const express = require("express");
 
 const versions = [
   {
     label: "latest",
-    module: require("express-physical")
+    module: require("express-physical"),
+    marshal: require("express-physical/src/marshal")
   },
   {
     label: "local",
-    module: require("../head")
+    module: require("../head"),
+    marshal: require("../head/src/marshal")
   }
 ];
 
@@ -18,6 +22,8 @@ const benchmarks = [];
 const setupBenchmarks = () => {
   initBenchmarkForHealthyResponse();
   initBenchmarkForUnhealthyResponse();
+  initBenchmarkForCheckEvaluation();
+  initBenchmarkForMarshalling();
 
   runNextTest();
 };
@@ -63,6 +69,73 @@ const initBenchmarkForUnhealthyResponse = () => {
     suite.add(`[${version.label}] | Create response | Unhealthy`, () =>
       physical.response(unhealthyResponse(physical))
     );
+  });
+  benchmarks.push(suite);
+};
+
+const initBenchmarkForMarshalling = () => {
+  const input = [
+    {
+      name: "Failing Test",
+      healthy: false,
+      actionable: false,
+      type: "SELF",
+      severity: "WARNING",
+      message: "Something is failing"
+    }
+  ];
+
+  const suite = createSuite();
+  versions.forEach(version => {
+    const marshal = version.marshal;
+    suite.add(`[${version.label}] | Marshal results`, () => {
+      marshal(input);
+    });
+  });
+  benchmarks.push(suite);
+};
+
+const initBenchmarkForCheckEvaluation = () => {
+  const suite = createSuite();
+  versions.forEach(version => {
+    const physical = version.module;
+    const passingCheck = () =>
+      physical.response({
+        name: "Sample passing check",
+        actionable: false,
+        healthy: true,
+        type: physical.type.SELF
+      });
+
+    const failingCheck = () =>
+      physical.response({
+        name: "Sample passing check",
+        actionable: false,
+        healthy: false,
+        severity: physical.severity.WARNING,
+        message: "Something failed",
+        type: physical.type.SELF
+      });
+
+    const app = express();
+    const path = "/health";
+    app.use(path, physical([passingCheck, failingCheck]));
+
+    suite.add(`[${version.label}] | Call healthcheck endpoint`, {
+      defer: true,
+      fn: deferred => {
+        return request(app)
+          .get(path)
+          .expect(500)
+          .then(response => {
+            deferred.resolve();
+          })
+          .catch(e => {
+            console.error(e);
+            deferred.reject();
+          });
+      }
+    });
   });
   benchmarks.push(suite);
 };
