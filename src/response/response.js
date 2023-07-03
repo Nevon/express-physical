37920693d9ec3b7bcf2ae8e7558864948c213daa
Tommy Brunn <tommy.brunn@gmail.com>
2023-07-03 09:49:33 +0200

    Replace spected with zod for schema validation
    Spected is unmaintained and shipping a vulnerable version of ramda.

--- src/response/response.js
+++ src/response/response.js
@@ -1,5 +1,6 @@
 "use strict";
 const R = require("ramda");
+const { isValidationErrorLike } = require("zod-validation-error");
 
 const InvalidHealthcheckResponse = require("./invalid-healthcheck-response");
 const validate = require("./validate");
@@ -19,28 +20,16 @@ const RESPONSE_FORMAT = {
 const createResponse = responseData =>
   R.pick(Object.keys(RESPONSE_FORMAT), responseData);
 
-const someFail = results =>
-  R.not(R.all(R.equals(true), Object.values(results)));
-
-const isMessage = R.compose(
-  R.not,
-  R.is(Boolean)
-);
-const firstMessage = R.compose(
-  R.head,
-  R.filter(isMessage),
-  R.values
-);
-
 module.exports = function(responseData) {
   const validatable = Object.assign({}, RESPONSE_FORMAT, responseData);
-  const validationResult = validate(validatable);
+  try {
+    validate(validatable);
+  } catch (error) {
+    if (!isValidationErrorLike(error)) {
+      throw error;
+    }
 
-  if (someFail(validationResult)) {
-    throw new InvalidHealthcheckResponse(
-      firstMessage(validationResult),
-      responseData
-    );
+    throw new InvalidHealthcheckResponse(error.message, responseData);
   }
 
   return createResponse(responseData);
--- src/response/validate.js
+++ src/response/validate.js
@@ -1,6 +1,7 @@
 "use strict";
 const R = require("ramda");
-const { validate } = require("spected");
+const { z } = require("zod");
+const { fromZodError } = require("zod-validation-error");
 
 const types = require("../constants/types");
 const severities = require("../constants/severities");
@@ -11,19 +12,6 @@ const DEPENDENT_ON_REQUIRED_TYPES = [
   types.EXTERNAL_DEPENDENCY
 ];
 
-const notEmpty = R.compose(
-  R.not,
-  R.isEmpty
-);
-const notNil = R.compose(
-  R.not,
-  R.isNil
-);
-const isString = R.is(String);
-const isBoolean = R.is(Boolean);
-const isObject = R.is(Object);
-const oneOf = collection => R.curry(R.includes)(R.__, R.keys(collection));
-
 const createSuffix = healthy =>
   `when healthcheck is ${healthy ? "healthy" : "unhealthy"}`;
 const SUFFIX_HEALTHY_MSG = createSuffix(true);
@@ -32,70 +20,86 @@ const SUFFIX_UNHEALTHY_MSG = createSuffix(false);
 const healthyCheckMessage = msg => `${msg} ${SUFFIX_HEALTHY_MSG}`;
 const unhealthyCheckMessage = msg => `${msg} ${SUFFIX_UNHEALTHY_MSG}`;
 
-const commonRules = input => ({
-  healthy: [[isBoolean, "Healthy should be a boolean"]],
-  info: [[R.either(R.isNil, isObject), "Info should be an object"]],
-  dependentOn: R.includes(input.type, DEPENDENT_ON_REQUIRED_TYPES)
-    ? [
-        [isString, "DependentOn should be a string"],
-        [
-          notEmpty,
-          `DependentOn is required when type is one of ${DEPENDENT_ON_REQUIRED_TYPES.join(
-            ", "
-          )}`
-        ]
-      ]
-    : [
-        [
-          R.isNil,
-          `DependentOn should be omitted when type is one of ${R.values(
-            R.omit(DEPENDENT_ON_REQUIRED_TYPES, types)
-          ).join(", ")}`
-        ]
-      ]
-});
+const commonRules = input =>
+  z.object({
+    healthy: z.boolean({
+      required_error: "Healthy is required",
+      invalid_type_error: "Healthy should be a boolean"
+    }),
+    info: z
+      .record(z.any(), {
+        invalid_type_error: "Info should be an object"
+      })
+      .optional(),
+    dependentOn: R.includes(input.type, DEPENDENT_ON_REQUIRED_TYPES)
+      ? z
+          .string({
+            required_error: `DependentOn is required when type is one of ${DEPENDENT_ON_REQUIRED_TYPES.join(
+              ", "
+            )}`,
+            invalid_type_error: "DependentOn should be a string"
+          })
+          .nonempty(
+            `DependentOn is required when type is one of ${DEPENDENT_ON_REQUIRED_TYPES.join(
+              ", "
+            )}`
+          )
+      : z
+          .undefined({
+            invalid_type_error: `DependentOn should be omitted when type is one of ${R.values(
+              R.omit(DEPENDENT_ON_REQUIRED_TYPES, types)
+            ).join(", ")}`
+          })
+          .nullable()
+  });
 
-const healthyRules = input => ({
-  name: [
-    [isString, healthyCheckMessage("Name should be a string")],
-    [notEmpty, healthyCheckMessage("Name should not be empty")]
-  ],
-  type: [
-    [
-      oneOf(types),
-      healthyCheckMessage(`Type should be one of ${R.keys(types).join(", ")}`)
-    ]
-  ],
-  actionable: [
-    [isBoolean, healthyCheckMessage("Actionable should be a boolean")]
-  ],
-  severity: [[R.isNil, healthyCheckMessage("Severity should be omitted")]]
-});
-
-const unhealthyRules = input => ({
-  severity: [
-    [notNil, unhealthyCheckMessage("Severity is required")],
-    [
-      oneOf(severities),
-      unhealthyCheckMessage(
-        `Severity should be one of ${R.keys(severities).join(", ")}`
-      )
-    ]
-  ],
-  message: [
-    [isString, unhealthyCheckMessage("Message should be a string")],
-    [notEmpty, unhealthyCheckMessage("Message is required")]
-  ]
-});
-
-const rules = input =>
-  Object.assign(
-    commonRules(input),
-    R.ifElse(({ healthy }) => healthy === true, healthyRules, unhealthyRules)(
-      input
+const healthyRules = z.object({
+  name: z
+    .string({
+      required_error: healthyCheckMessage("Name is required"),
+      invalid_type_error: healthyCheckMessage("Name should be a string")
+    })
+    .nonempty(healthyCheckMessage("Name should not be empty")),
+  type: z.enum(R.values(types), {
+    required_error: healthyCheckMessage("Type is required"),
+    invalid_type_error: healthyCheckMessage(
+      `Type should be one of ${R.keys(types).join(", ")}`
     )
+  }),
+  actionable: z.boolean({
+    required_error: healthyCheckMessage("Actionable is required"),
+    invalid_type_error: healthyCheckMessage("Actionable should be a boolean")
+  }),
+  severity: z
+    .undefined({
+      invalid_type_error: healthyCheckMessage("Severity should be omitted")
+    })
+    .optional()
+});
+
+const unhealthyRules = z.object({
+  severity: z.enum(R.values(severities), {
+    required_error: unhealthyCheckMessage("Severity is required"),
+    invalid_type_error: unhealthyCheckMessage(
+      `Severity should be one of ${R.keys(severities).join(", ")}`
+    )
+  }),
+  message: z.string({
+    required_error: unhealthyCheckMessage("Message is required"),
+    invalid_type_error: unhealthyCheckMessage("Message should be a string")
+  })
+});
+
+const rules = input => {
+  return commonRules(input).merge(
+    input.healthy === true ? healthyRules : unhealthyRules
   );
+};
 
-const validator = R.curry(validate(() => true, R.head));
-
-module.exports = input => validator(rules(input))(input);
+module.exports = input => {
+  try {
+    return rules(input).parse(input);
+  } catch (error) {
+    throw fromZodError(error, { prefix: "", prefixSeparator: "" });
+  }
+};
